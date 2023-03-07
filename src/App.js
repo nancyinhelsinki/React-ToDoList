fd9bdfdd68c451a848abff61569026b80f8ebee7
Ngan Nguyen <huynhngan281195@gmail.com>
2023-03-07 22:21:52 +0200

    split component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import React from "react";
 import "./App.css";
 import ToDoList from "./components/ToDoList";
 
 function App() {
+  const [description, setDescription] = React.useState("");
+  const [date, setDate] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
+
+  const handleAddToDo = () => {
+    setTodos([{ description, date }, ...todos]);
+    console.log(todos);
+    setDescription("");
+    setDate("");
+  };
+  const handleDeleteToDo = (index) => {
+    const result = [...todos];
+    result.splice(index, 1);
+    setTodos(result);
+  };
   return (
     <div className="App">
-      <ToDoList />
+      <h1>My ToDos</h1>
+      <input
+        placeholder="Description"
+        value={description}
+        onChange={(event) => {
+          console.log(event);
+          setDescription(event.target.value);
+        }}
+      />
+      <input
+        type="date"
+        placeholder="Date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+
+      <button onClick={handleAddToDo}>Add Todos</button>
+      <ToDoList todoItems={todos} deleteTodo={handleDeleteToDo} />
     </div>
   );
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,43 +1,10 @@
 // can type React.useState or import React, {useState} from 'react';
 import React from "react";
-export default function ToDoList() {
+export default function ToDoList(props) {
   //--todolist-- const [description, setDescription] = React.useState("");
 
-  const [description, setDescription] = React.useState("");
-  const [date, setDate] = React.useState("");
-  const [todos, setTodos] = React.useState([]);
-
-  const handleAddToDo = () => {
-    setTodos([{ description, date }, ...todos]);
-    console.log(todos);
-    setDescription("");
-    setDate("");
-  };
-  const handleDeleteToDo = (index) => {
-    const result = [...todos];
-    result.splice(index, 1);
-    setTodos(result);
-  };
   return (
     <div>
-      <h1>My ToDos</h1>
-      <input
-        placeholder="Description"
-        value={description}
-        onChange={(event) => {
-          console.log(event);
-          setDescription(event.target.value);
-        }}
-      />
-      <input
-        type="date"
-        placeholder="Date"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-      />
-
-      <button onClick={handleAddToDo}>Add Todos</button>
-
       <table>
         <th>
           <td>Description</td>
@@ -46,7 +13,7 @@ export default function ToDoList() {
         </th>
 
         <tbody>
-          {todos.map((todo, index) => (
+          {props.todoItems.map((todo, index) => (
             <tr key={index}>
               <td>{todo.description}</td>
               <td>{todo.date}</td>
@@ -54,7 +21,7 @@ export default function ToDoList() {
                 <button
                   onClick={(event) => {
                     console.log(event);
-                    handleDeleteToDo(index);
+                    props.deleteTodo(index);
                   }}
                 >
                   Delete
